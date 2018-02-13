b09c12af19e085b0f9855584cb527b06cec3f485
Michael Worobec <michael.worobec@tundra.com.au>
2018-02-14 08:28:46 +1100

    WIP

--- src/_functions.scss.js
+++ src/_functions.scss.js
@@ -7,33 +7,49 @@ const selectors = content.match(/[a-z0-9-(.)]+/g);
 */
 
 module.exports = {
+
+  /**
+   * Class list
+   *
+   * @return {SassList}
+   */
   _f_class_list() {
     const arr = [...new Set(['m(.1..3)', 'text(left...center)'])];
-    const $class_list = new sass.types.List(arr.length);
+    const $classList = new sass.types.List(arr.length);
 
     for (i = 0; i < arr.length; i++) {
-      $class_list.setValue(i, new sass.types.String(arr[i]));
+      $classList.setValue(i, new sass.types.String(arr[i]));
     }
-
-    return $class_list;
+    return $classList;
   },
 
-  _f_util($class_name) {
-    const className = $class_name.getValue();
+  /**
+   * Util
+   *
+   * @param  {SassString} $className [description]
+   * @return {SassString}
+   */
+  _f_util($className) {
+    const className = $className.getValue();
     const util = className.substring(0, className.indexOf('('));
 
     return new sass.types.String(util);
   },
 
-  _f_exp($class_name) {
-    const className = $class_name.getValue();
-    const exp = className.slice(className.indexOf('(') + 1, -1).split('.');
-    const $exp = new sass.types.List(exp.length);
+  /**
+   * Exp list
+   *
+   * @param  {SassString} $className [description]
+   * @return {SassList}
+   */
+  _f_exp_list($className) {
+    const className = $className.getValue();
+    const expList = className.slice(className.indexOf('(') + 1, -1).split('.');
+    const $expList = new sass.types.List(expList.length);
 
-    for (i = 0; i < exp.length; i++) {
-      $exp.setValue(i, new sass.types.String(exp[i]));
+    for (i = 0; i < expList.length; i++) {
+      $expList.setValue(i, new sass.types.String(expList[i]));
     }
-
-    return $exp;
+    return $expList;
   }
 };
